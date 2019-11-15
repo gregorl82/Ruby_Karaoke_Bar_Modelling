8fffde2a0592497580f5dbb0ff3539699d2d982f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-15 20:27:40 +0000

    Adds cash attribute to Guest class

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,11 +7,15 @@ MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 class GuestTest < MiniTest::Test
 
   def setup()
-    @guest = Guest.new("Jen")
+    @guest = Guest.new("Jen", 30)
   end
 
   def test_can_create_guest_and_get_name()
     assert_equal("Jen", @guest.name)
+  end
+
+  def test_can_get_guess_cash()
+    assert_equal(30, @guest.cash)
   end
 
 end
